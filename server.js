3dc19e59ba568981449843c298125a4bf1cc8cac
Pieter Rees <pieter@pieterrees.nl>
2020-01-15 21:27:35 +0100

    init

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const MongoClient = require('mongodb').MongoClient;
+const bodyParser = require('body-parser');
+const app = express();
+
+const port = 5678;
+
+app.listen(port, () => {
+  console.log('We are live on ' + port);
+});
+
+require('./app/routes')(app, {});
