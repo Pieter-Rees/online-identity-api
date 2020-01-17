612fe4cbb95a2d981b4f376a932f12b1f20a1a79
Pieter Rees <pieter@pieterrees.nl>
2020-01-17 13:47:58 +0100

    changed port

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors());
 
-const port = process.env.PORT || 8080; // set our port
+const port = process.env.PORT || 5050; // set our port
 
 // ROUTES FOR OUR API
 // =============================================================================
